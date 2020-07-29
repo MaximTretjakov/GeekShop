e4ed3c7810fc968b85f39d247fd59bbac0248010
MAXIM <victor-kovalov@yandex.ru>
2020-07-29 15:34:07 +0300

    Add dynamics to template

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -1,8 +1,19 @@
 from django.urls import path
-from mainapp.views import HomePageView, ProductsPageView, ContactPageView
+
+from mainapp.views import (
+    HomePageView, ProductsPageView, ContactPageView,
+    ProductsPageAllView, ProductsPageHomeView, ProductsPageOfficeView,
+    ProductsPageModernView, ProductsPageClassicView
+)
 
 urlpatterns = [
     path('', HomePageView.as_view(), name='home'),
     path('products/', ProductsPageView.as_view(), name='products'),
     path('contact/', ContactPageView.as_view(), name='contact'),
+
+    path('products/all/', ProductsPageAllView.as_view(), name='products_all'),
+    path('products/home/', ProductsPageHomeView.as_view(), name='products_house'),
+    path('products/office/', ProductsPageOfficeView.as_view(), name='products_office'),
+    path('products/modern/', ProductsPageModernView.as_view(), name='products_modern'),
+    path('products/classic/', ProductsPageClassicView.as_view(), name='products_classic'),
 ]
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,17 +1,83 @@
 from django.shortcuts import render
 from django.views.generic import View
+from django.conf import settings
+import json
+import logging
+
+logging.basicConfig(filename='example.log',level=logging.DEBUG)
+
+
+class DataMixin:
+    def get_product_categories(self):
+        return [
+            {'href': 'mainapp:products_all', 'name': 'все', 'reverse_url': 'products_all'},
+            {'href': 'mainapp:products_house', 'name': 'дом', 'reverse_url': 'products_house'},
+            {'href': 'mainapp:products_office', 'name': 'офис', 'reverse_url': 'products_office'},
+            {'href': 'mainapp:products_modern', 'name': 'модерн', 'reverse_url': 'products_modern'},
+            {'href': 'mainapp:products_classic', 'name': 'классика', 'reverse_url': 'products_classic'},
+        ]
 
 
 class HomePageView(View):
+    def get_dynamic_data(self):
+        try:
+            file_path = settings.BASE_DIR + '\\mainapp\\dynamic_data.json'
+            with open(file_path, 'r', encoding="UTF-8") as f:
+                return json.load(f)
+        except Exception as e:
+            logging.debug(e)
+
     def get(self, request):
-        return render(request, 'mainapp/index.html')
+        return render(request, 'mainapp/index.html', {'title': 'Магазин', 'dynamic_data': self.get_dynamic_data()})
 
 
 class ContactPageView(View):
     def get(self, request):
-        return render(request, 'mainapp/contact.html')
+        return render(request, 'mainapp/contact.html', {'title': 'Контакты'})
 
 
-class ProductsPageView(View):
+class ProductsPageView(DataMixin, View):
     def get(self, request):
-        return render(request, 'mainapp/products.html')
+        return render(
+            request, 'mainapp/products.html', {'title': 'Продукты', 'links_menu': super().get_product_categories()}
+        )
+
+
+class ProductsPageAllView(DataMixin, View):
+    def get(self, request):
+        return render(
+            request, 'mainapp/products.html',
+            {'title': 'Продукты все', 'links_menu': super().get_product_categories()}
+        )
+
+
+class ProductsPageHomeView(DataMixin, View):
+    def get(self, request):
+        return render(
+            request, 'mainapp/products.html',
+            {'title': 'Продукты для дома', 'links_menu': super().get_product_categories()}
+        )
+
+
+class ProductsPageOfficeView(DataMixin, View):
+    def get(self, request):
+        return render(
+            request, 'mainapp/products.html',
+            {'title': 'Продукты для офиса', 'links_menu': super().get_product_categories()}
+        )
+
+
+class ProductsPageModernView(DataMixin, View):
+    def get(self, request):
+        return render(
+            request, 'mainapp/products.html',
+            {'title': 'Продукты модерн', 'links_menu': super().get_product_categories()}
+        )
+
+
+class ProductsPageClassicView(DataMixin, View):
+    def get(self, request):
+        return render(
+            request, 'mainapp/products.html',
+            {'title': 'Продукты классика', 'links_menu': super().get_product_categories()}
+        )
