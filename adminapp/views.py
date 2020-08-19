ec08a5fb8f68f55f1dc13da11daec77205c7e85f
MAXIM <victor-kovalov@yandex.ru>
2020-08-19 22:19:34 +0300

    Add lesson-7

---
+++ adminapp/views.py
@@ -0,0 +1,108 @@
+from django.shortcuts import render
+from django.views.generic import View
+
+
+class UsersPageView(View):
+    def post(self, request):
+        pass
+
+    def get(self, request):
+        pass
+
+
+class UserCreatePageView(View):
+    def post(self, request):
+        pass
+
+    def get(self, request):
+        pass
+
+
+class UserUpdatePageView(View):
+    def post(self, request, **kwargs):
+        pass
+
+    def get(self, request):
+        pass
+
+
+class UserDeletePageView(View):
+    def post(self, request, **kwargs):
+        pass
+
+    def get(self, request):
+        pass
+
+
+class CategoriesPageView(View):
+    def post(self, request):
+        pass
+
+    def get(self, request):
+        pass
+
+
+class CategoryCreatePageView(View):
+    def post(self, request):
+        pass
+
+    def get(self, request):
+        pass
+
+
+class CategoryUpdatePageView(View):
+    def post(self, request, **kwargs):
+        pass
+
+    def get(self, request):
+        pass
+
+
+class CategoryDeletePageView(View):
+    def post(self, request, **kwargs):
+        pass
+
+    def get(self, request):
+        pass
+
+
+class ProductsPageView(View):
+    def post(self, request, **kwargs):
+        pass
+
+    def get(self, request):
+        pass
+
+
+class ProductCreatePageView(View):
+    def post(self, request):
+        pass
+
+    def get(self, request):
+        pass
+
+
+class ProductReadPageView(View):
+    def post(self, request, **kwargs):
+        pass
+
+    def get(self, request):
+        pass
+
+
+class ProductUpdatePageView(View):
+    def post(self, request, **kwargs):
+        pass
+
+    def get(self, request):
+        pass
+
+
+class ProductDeletePageView(View):
+    def post(self, request, **kwargs):
+        pass
+
+    def get(self, request):
+        pass
+
+
--- shop/urls.py
+++ shop/urls.py
@@ -4,10 +4,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
     path('', include('mainapp.urls', namespace='mainapp')),
     path('auth/', include('authapp.urls', namespace='authapp')),
     path('basket/', include('basketapp.urls', namespace='basketapp')),
+    path('admin/', include('adminapp.urls', namespace='adminapp')),
 ]
 
 if settings.DEBUG:
